03e574d792788aebb0c4b8d6d690bf2ae74ce978
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2026-04-19 14:32:32 -0500

    add home page content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,26 @@
+import wiOutline from "../../assets/images/wisconsin_outline.png";
+
+import styles from "./Home.module.css";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1 className={styles.heading}>Fullstack web developer</h1>
+
+      <p className={styles.hook}>
+        Driven by curiosity and a hands-on approach to learning, I create clean,
+        user-friendly applications.
+      </p>
+
+      <p>
+        Located in{" "}
+        <span className={styles.wisconsin}>
+          Wisconsin
+          <img src={wiOutline} />
+        </span>
+      </p>
+    </>
+  );
 }
 
 export default Home;
