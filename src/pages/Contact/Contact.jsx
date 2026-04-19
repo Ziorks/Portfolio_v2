be68b303c69ed5b88c8eebcee08a44697417dc8d
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2026-04-19 13:41:13 -0500

    add contact page content

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,21 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
+
+import styles from "./Contact.module.css";
+
 function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <div className={styles.contactContainer}>
+      <a href="https://www.github.com/Ziorks" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://www.linkedin.com/in/frank-a-scott/" target="_blank">
+        <FaLinkedin />
+      </a>
+      <a href="mailto:franksixer@gmail.com" target="_blank">
+        franksixer@gmail.com
+      </a>
+    </div>
+  );
 }
 
 export default Contact;
